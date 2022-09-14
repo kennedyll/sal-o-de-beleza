e1b1c6384ac897edcafb84f9f86a9daa803edf55
alunoctg <alunoctg@fiern.local>
2022-09-14 08:33:55 -0300

    primeiro commit

---
+++ salao_de_beleza/salao_de_beleza/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for salao_de_beleza project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'salao_de_beleza.settings')
+
+application = get_wsgi_application()
